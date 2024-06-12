9ced2e0ac08af1e3e40f8c023f9b557743ee3e48
kjs <qojsk12@naver.com>
2024-06-12 17:42:52 +0900

    adfasdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './routes/home';
 import {createGlobalStyle} from 'styled-components';
 import reset from 'styled-reset';
 import PhysicalFitnessAssessment from './routes/PhysicalFitnessAssessment';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'physical',
+        element: <PhysicalFitnessAssessment />,
+      },
+    ],
+  },
+]);
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
@@ -11,24 +28,17 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    font-family: "Jua", sans-serif;
+  font-family: "Jua", sans-serif;
   }
-`;
+  `;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="physical" element={<PhysicalFitnessAssessment />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
-// test
+
 export default App;
