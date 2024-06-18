602f81a054f4724eef5a05728fd0d240c8280b20
kjs <qojsk12@naver.com>
2024-06-18 16:18:43 +0900

    ad

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,7 +3,6 @@ import X4 from '../assets/images/001.png';
 import X3 from '../assets/images/002.png';
 import styled from 'styled-components';
 import SnsItem from '../components/SnsItem';
-import Background from '../assets/images/005.png';
 
 const imageUrls = [X4, X3];
 
@@ -12,8 +11,6 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: space-around;
   box-shadow: inset 1px 3px 10px 3px #c1c1c1;
-  background-image: url(${Background});
-  background-size: cover;
 `;
 
 const SnsBox = styled.div`
