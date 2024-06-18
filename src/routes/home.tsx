f00115dfffd569060ff4f131829f61ec99c4921c
kjs <qojsk12@naver.com>
2024-06-18 10:26:47 +0900

    ad

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,6 +3,7 @@ import X4 from '../assets/images/001.png';
 import X3 from '../assets/images/002.png';
 import styled from 'styled-components';
 import SnsItem from '../components/SnsItem';
+import Background from '../assets/images/005.png';
 
 const imageUrls = [X4, X3];
 
@@ -11,6 +12,8 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: space-around;
   box-shadow: inset 1px 3px 10px 3px #c1c1c1;
+  background-image: url(${Background});
+  background-size: cover;
 `;
 
 const SnsBox = styled.div`
@@ -30,7 +33,7 @@ export default function Home() {
   return (
     <Wrapper>
       <SnsBox>
-        <Title>SNS로 소식 받기</Title>
+        <Title>시흥체력인증센터 소식 받기</Title>
         <SnsItem />
       </SnsBox>
       <ImageSlider images={imageUrls} />
