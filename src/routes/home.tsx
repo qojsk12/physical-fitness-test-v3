ca16a3985011498c5d22be3bca87d4376177c5af
kjs <qojsk12@naver.com>
2024-06-20 17:50:17 +0900

    ad

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -8,8 +8,9 @@ const imageUrls = [X4, X3];
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
   box-shadow: inset 1px 3px 10px 3px #c1c1c1;
 `;
 
@@ -29,11 +30,11 @@ const Title = styled.div`
 export default function Home() {
   return (
     <Wrapper>
+      <ImageSlider images={imageUrls} />
       <SnsBox>
         <Title>시흥체력인증센터 소식 받기</Title>
         <SnsItem />
       </SnsBox>
-      <ImageSlider images={imageUrls} />
     </Wrapper>
   );
 }
