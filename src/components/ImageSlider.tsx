4b807da7e5a1eb226aab472fbc26fa75bb9a128b
kjs <qojsk12@naver.com>
2024-06-21 09:13:09 +0900

    ad

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -17,6 +17,7 @@ const SliderContainer = styled.div`
   overflow: hidden;
   border-radius: 10px;
   position: relative;
+  margin-top: 30px;
   .slick-dots {
     position: absolute;
     transform: translateX(-50%);
